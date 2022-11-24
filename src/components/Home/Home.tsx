668ec7223ce9331ccb6506252c1020794eda2638
xHenrique22 <henrique.oliveira9823@gmail.com>
2022-11-24 12:03:14 -0300

    feat: implemented grid

---
+++ src/components/Home/Home.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import "./Home.css";
+import {Grid} from "@material-ui/core";
+
+function Home(){
+    return(
+        <>
+        <Grid container>
+            <Grid item>
+
+            </Grid>
+            <Grid item container>
+                <Grid item>
+
+
+                </Grid>
+                <Grid item>
+
+
+                </Grid>
+            </Grid>
+        </Grid>
+        </>
+    )
+}
+
+export default Home;
