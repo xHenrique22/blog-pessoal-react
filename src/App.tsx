d06fc495bbfdefc31feaa33c15f090e69ca7b2dc
xHenrique22 <henrique.oliveira9823@gmail.com>
2022-12-02 23:46:25 -0300

    minor fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,30 @@
 import React from 'react';
-import './App.css';
-import Home from './paginas/home/Home';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './components/estaticos/navbar/Navbar';
 import Footer from './components/estaticos/footer/Footer';
-
-
+import Home from './paginas/home/Home';
+import './App.css';
+import Login from './paginas/login/Login';
+import CadastroUsuario from './paginas/cadastroUsuario/CadastroUsuario';
 
 
 function App() {
   return (
-    <>
+    <Router>
       <Navbar />
-      <Home />
+      <Routes>
+
+        <Route path="/" element={<Home />} />
+
+        <Route path="/home" element={<Home />} />
+
+        <Route path="/login" element={<Login />} />
+
+        <Route path="/cadastro" element={<CadastroUsuario />} />
+        
+      </Routes>
       <Footer />
-    </>
+    </Router>
 
   );
 }
